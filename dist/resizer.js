378857a567ff4bc837d7e4e8331dc8e27edd41e3
Edward Knowles <mail@eknowles.com>
2017-05-02 15:42:56 +0100

    chore(build): add dist for coveralls
    dirty I know, I'll remove soon

---
+++ dist/resizer.js
@@ -0,0 +1,126 @@
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var Resizer = function () {
+    function Resizer(containerSelector, resizerOptions) {
+        if (resizerOptions === void 0) {
+            resizerOptions = {};
+        }
+        this.containerSelector = containerSelector;
+        this.offsetX = 0;
+        this._dragging = false;
+        if (!containerSelector) {
+            throw new Error('Missing param containerSelector');
+        }
+        this.options = _extends(Resizer.defaultOptions, resizerOptions, {});
+        if (typeof containerSelector === 'string') {
+            this.container = document.querySelector(containerSelector);
+        } else {
+            this.container = containerSelector;
+        }
+        if (!this.container) {
+            throw new Error("Can not find element from selector " + containerSelector);
+        }
+        this.target = this.container.firstElementChild;
+        if (this.container.Resizer) {
+            this.remove();
+        }
+        this.setup();
+    }
+    Resizer.createHandle = function () {
+        var el = document.createElement('div');
+        el.dataset.rzHandle = '';
+        el.style.cursor = 'ew-resize';
+        return el;
+    };
+    Resizer.createGhost = function () {
+        var el = document.createElement('div');
+        el.style.position = 'absolute';
+        el.style.top = '0';
+        el.style.bottom = '0';
+        el.style.display = 'none';
+        return el;
+    };
+    Resizer.prototype.remove = function () {
+        delete this.container.Resizer;
+        this.container.style.position = null;
+        this.handle.remove();
+        this.target.style.flex = null;
+    };
+    Resizer.prototype.setup = function () {
+        var _this = this;
+        this.setupDom();
+        this.handle.addEventListener('mousedown', function (e) {
+            return _this.onDown(e);
+        });
+        this.container.addEventListener('mouseup', function (e) {
+            return _this.onUp(e);
+        });
+        this.container.addEventListener('mousemove', function (e) {
+            return _this.onMove(e);
+        });
+        this.container.Resizer = this;
+    };
+    Resizer.prototype.setupDom = function () {
+        this.container.style.position = 'relative';
+        this.handle = Resizer.createHandle();
+        this.ghost = Resizer.createGhost();
+        this.handle.appendChild(this.ghost);
+        this.container.insertBefore(this.handle, this.target.nextElementSibling);
+    };
+    Object.defineProperty(Resizer.prototype, "dragging", {
+        get: function () {
+            return this._dragging;
+        },
+        set: function (value) {
+            if (this.dragging) {
+                this.ghost.style.display = 'none';
+                this.target.style.flex = "0 0 " + this.handleX + "px";
+            } else {
+                this.ghost.style.display = 'block';
+            }
+            this._dragging = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Resizer.prototype.setHandleX = function (value) {
+        if (value < 0) {
+            value = 0;
+        }
+        if (value > this.container.clientWidth) {
+            value = this.container.clientWidth;
+        }
+        this.ghost.style.left = value + "px";
+        this.handleX = value;
+    };
+    Resizer.prototype.onDown = function (e) {
+        e.preventDefault();
+        if (!this.dragging) {
+            this.offsetX = e.offsetX;
+            this.setHandleX(e.pageX - this.container.getBoundingClientRect().left - this.offsetX);
+            this.dragging = true;
+        }
+    };
+    Resizer.prototype.onUp = function (e) {
+        e.preventDefault();
+        if (this.dragging) {
+            this.setHandleX(e.pageX - this.container.getBoundingClientRect().left - this.offsetX);
+            this.dragging = false;
+        }
+    };
+    Resizer.prototype.onMove = function (e) {
+        e.preventDefault();
+        if (this.dragging) {
+            var x = e.pageX - this.container.getBoundingClientRect().left - this.offsetX;
+            if (e.shiftKey) {
+                x = Math.ceil(x / 20) * 20;
+            }
+            this.setHandleX(x);
+        }
+    };
+    return Resizer;
+}();
+Resizer.defaultOptions = {
+    width: 8
+};
+//# sourceMappingURL=resizer.js.map
